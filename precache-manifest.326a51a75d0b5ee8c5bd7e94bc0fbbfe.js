78ffbf3eb5f1c1ad54579eecf96fec19d73225cd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-24 20:59:27 +0000

    Deploy anjrot01/practica-bebidas to github.com/anjrot01/practica-bebidas.git:gh-pages

---
+++ precache-manifest.326a51a75d0b5ee8c5bd7e94bc0fbbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1aeb034e91d4af80d4a67206df5c9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a04123df5282782c6c",
+    "url": "/static/css/main.9040a63c.chunk.css"
+  },
+  {
+    "revision": "7d897dec8aec13152d5d",
+    "url": "/static/js/2.b910de57.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/static/js/2.b910de57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a04123df5282782c6c",
+    "url": "/static/js/main.1a5d3109.chunk.js"
+  },
+  {
+    "revision": "3cda9f0a015c04d565ee",
+    "url": "/static/js/runtime-main.b9b28f7a.js"
+  }
+]);
